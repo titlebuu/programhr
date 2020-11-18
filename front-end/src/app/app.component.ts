7c07016de5f69ea1c6329be0ee5e476852288945
Teerapat Wongsa <teerapat@rotaryeng.co.th>
2020-11-18 09:47:50 +0700

    update fontend

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent implements OnInit {
     { field: 'AttendanceType', header: 'Attendance Type' },
     { field: 'Houre', header: 'Hours' }
 
-];
+  ];
 
   constructor(private http: HttpClient, _excelService: ExcelService) {
 
@@ -54,7 +54,7 @@ export class AppComponent implements OnInit {
 
     const date1 = moment(this.start_date).format('YYYY-MM-DD');
     const date2 = moment(this.to_date).format('YYYY-MM-DD');
-    
+
     this.data = await this.query('/api/listdata', {
       date1: date1,
       date2: date2
@@ -76,18 +76,12 @@ export class AppComponent implements OnInit {
   }
 
   public exportAsExcelFile(json: any[], headerText: any[], excelFileName: string): void {
-
     var worksheet = XLSX.utils.json_to_sheet(headerText, { header: [], skipHeader: true });
-
     XLSX.utils.sheet_add_json(worksheet, json, { skipHeader: true, origin: "A2" });
-
     const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-
     const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-
     this.saveAsExcelFile(excelBuffer, excelFileName);
   }
-
   private saveAsExcelFile(buffer: any, fileName: string): void {
     const data: Blob = new Blob([buffer], {
       type: EXCEL_TYPE
@@ -105,34 +99,34 @@ export class AppComponent implements OnInit {
     }
   }
 
-//   exportPdf() {
-//     import("jspdf").then(jsPDF => {
-//         import("jspdf-autotable").then(x => {
-//             const doc = new jsPDF.default(0,0);
-//             doc.autoTable(this.exportColumns, this.products);
-//             doc.save('products.pdf');
-//         })
-//     })
-// }
+  //   exportPdf() {
+  //     import("jspdf").then(jsPDF => {
+  //         import("jspdf-autotable").then(x => {
+  //             const doc = new jsPDF.default(0,0);
+  //             doc.autoTable(this.exportColumns, this.products);
+  //             doc.save('products.pdf');
+  //         })
+  //     })
+  // }
 
-// exportExcel() {
-//     import("xlsx").then(xlsx => {
-//         const worksheet = xlsx.utils.json_to_sheet(this.data);
-//         const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-//         const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
-//         this.saveAsExcelFile(excelBuffer, "data");
-//     });
-// }
+  // exportExcel() {
+  //     import("xlsx").then(xlsx => {
+  //         const worksheet = xlsx.utils.json_to_sheet(this.data);
+  //         const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+  //         const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+  //         this.saveAsExcelFile(excelBuffer, "data");
+  //     });
+  // }
 
-// saveAsExcelFile(buffer: any, fileName: string): void {
-//     import("file-saver").then(FileSaver => {
-//         let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-//         let EXCEL_EXTENSION = '.xlsx';
-//         const data: Blob = new Blob([buffer], {
-//             type: EXCEL_TYPE
-//         });
-//         FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
-//     });
-// }
+  // saveAsExcelFile(buffer: any, fileName: string): void {
+  //     import("file-saver").then(FileSaver => {
+  //         let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  //         let EXCEL_EXTENSION = '.xlsx';
+  //         const data: Blob = new Blob([buffer], {
+  //             type: EXCEL_TYPE
+  //         });
+  //         FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  //     });
+  // }
 }
 
