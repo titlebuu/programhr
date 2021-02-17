be0b43ce017e864d4271e296426fbf6a24ab73fd
titlebuu <titlebuu@gmail.com>
2021-02-17 17:49:00 +0700

    upload

--- front-end/src/app/hr/addcode/addcode.component.ts
+++ front-end/src/app/hr/addcode/addcode.component.ts
@@ -5,7 +5,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Message, MessageService } from 'primeng/api';
 import { ConfirmationService } from 'primeng/api';
 import * as XLSX from 'xlsx';
-
+type AOA = any[][];
 @Component({
   selector: 'app-addcode',
   templateUrl: './addcode.component.html',
@@ -13,7 +13,8 @@ import * as XLSX from 'xlsx';
   providers: [ConfirmationService, MessageService]
 })
 export class AddcodeComponent implements OnInit {
-  data:[][];
+  data: AOA = [[], []];
+
   constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
@@ -23,22 +24,26 @@ export class AddcodeComponent implements OnInit {
     this.router.navigate([``]);
   }
 
-  onFileChange(evt: any){
-    const target : DataTransfer = <DataTransfer>(evt.target);
-
-    if(target.files.length !== 1) throw new Error('Cannot use multiple files');
-
+  onFileChange(evt) {
+    /* wire up file reader */
+    debugger
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
     const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
 
-    reader.onload = (e: any)=>{
-      const bstr: string = e.target.result
-      const wb: XLSX.WorkBook = XLSX.read(bstr,{type: 'binary'})
-      const wsname : string = wb.SheetNames[0]
-      const ws: XLSX.WorkSheet = wb.Sheets[wsname]
-      console.log(ws);
-      this.data = (XLSX.utils.sheet_to_json(ws,{header:1}))
-      console.log(this.data)
-    }
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      console.log(this.data);
+    };
+    reader.readAsBinaryString(target.files[0]);
   }
 
 }
